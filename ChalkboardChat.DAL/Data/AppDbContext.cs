a1188e8ae52ed3e66273feeb62c10b8565a67b5a
LudvigSHub <Ludvig.salovuo@gmail.com>
2026-02-12 12:57:06 +0100

    databas migrations

---
+++ ChalkboardChat.DAL/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChalkboardChat.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChalkboardChat.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MessageModel> Messages { get; set; }
+
+        
+    }
+}
