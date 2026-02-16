4566647e5302c310033b462307e415c988fb9d61
LudvigSHub <Ludvig.salovuo@gmail.com>
2026-02-16 15:04:23 +0100

    Lagt till: -IMessageRepository -MessageRepository

---
+++ ChalkboardChat.DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChalkboardChat.DAL.Data;
+using ChalkboardChat.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChalkboardChat.DAL.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MessageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MessageModel>> GetLatestAsync()
+        {
+            return await _context.Messages
+                .OrderByDescending(m => m.Date)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(MessageModel message)
+        {
+            await _context.Messages.AddAsync(message);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
