7e0aaf122fb195568f221eac1c606a2fbba5f00c
AngelosLekkas <lekkas.angelos@gmail.com>
2026-02-17 18:53:34 +0100

    BLL implementation

---
+++ ChalkboardChat.BLL/DTOs/CreateMessageDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChalkboardChat.BLL.DTOs
+{
+    public class CreateMessageDto
+    {
+        public string Message { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+    }
+}
---
+++ ChalkboardChat.BLL/DTOs/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChalkboardChat.BLL.DTOs
+{
+    internal class UpdateUserDto
+    {
+    }
+}
